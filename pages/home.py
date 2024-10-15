15f1dc4c02f67dbae65e5eef3e208319250b6056
kostasafe <22775121+kostasafe@users.noreply.github.com>
2024-10-15 14:51:00 +0300

    added file reader

--- pages/home.py
+++ pages/home.py
@@ -8,7 +8,23 @@ import pandas as pd
 
 def show_Home() :
 
-    st.write("Welcome to the Linked Data App! 🚀")
-    st.write("We're crafting something amazing here! Stay tuned as we prepare to unveil powerful tools and insights. In the meantime, explore our GitHub or check back soon for exciting updates!")
+        st.markdown(
+        """
+        <style>
+        .title {
+            font-size: 70px;
+            text-align: center;
+            margin-bottom: 0px;
+            margin-top: 0px;
+        }
+         </style>
+        """, unsafe_allow_html=True
+        )
 
-    
+        st.markdown('<div class="title">Welcome to our Linked Data Analysis Application</div>', unsafe_allow_html=True)
+        st.header("Please upload your desired dataset:", divider="orange")
+        uploaded_files = st.file_uploader("Choose :green[CSV] or :green[xml] files", type={"csv", "xml"}, accept_multiple_files=True)
+        #for uploaded_file in uploaded_files:
+            #bytes_data = uploaded_file.read()
+            #st.write("filename:", uploaded_file.name)
+            #st.write(bytes_data)
